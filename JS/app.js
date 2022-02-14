b723ceff5ccb1bfa259085f8ad6db4366a9811fc
YangSeonmo <tnwlr48@gmail.com>
2022-02-14 16:41:24 +0900

    Greeting user
    greeting user when user enter name,
    1. Form add Hidden class
    2. change inner text of h1
    3. remove hidden class of h1

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,7 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
+const greeting = document.querySelector("#greeting");
 
 /*  
 위 3줄은
@@ -9,10 +10,21 @@ const loginButton = loginForm.querySelector("#login-form button");
 과 같다
 */
 
-function handleLoginBtnClick(){
-    console.dir(loginInput.value);
-    console.log("click!!!!");
+const HIDDEN_CLASSNAME = "hidden"
+
+
+function onLoginsubmit(event){
+    event.preventDefault();
+    const username= loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    console.log("username");
+    greeting.innerText= `hello ${username}!`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
+/* event에는 방금 실행된 submit event에대한 정보가 담겨있다. */
 
-loginButton.addEventListener("click", handleLoginBtnClick);
+/* preventDefault() <-- 어떤 function에 대한 브라우저가 기본적으로 수행하는 동작을 막다.
+= submit의 default event인 새로고침을 prevent하다 */
+
+loginForm.addEventListener("submit", onLoginsubmit);
