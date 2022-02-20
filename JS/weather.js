7286f61ee3ab8ee065778892cd3a5fe798550a8c
YangSeonmo <tnwlr48@gmail.com>
2022-02-20 23:39:22 +0900

    why not showing weather and location!!!!
    !!!! github exist it but gh-pages deployment change doesn't have weather.js!!!!

--- JS/weather.js
+++ JS/weather.js
@@ -1,11 +1,11 @@
 
-const API_KEY= "3c6e59be400a550fcc86c27b86e1fc0e"
+const API__KEY= "3c6e59be400a550fcc86c27b86e1fc0e"
 
 
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API__KEY}&units=metric`;
   fetch(url).then((response) => response.json())
   .then((data)=>{
       const weather=document.querySelector("#weather span:first-child");
